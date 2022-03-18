986225a1f2fa2af5e67e819db982206ac05cfce5
PiotrMakarewicz <makarepio@gmail.com>
2022-03-18 12:01:22 +0100

    heroku deployment try 16

--- src/main/java/pl/edu/agh/pmakarewicz/moviestars/MoviestarsApplication.java
+++ src/main/java/pl/edu/agh/pmakarewicz/moviestars/MoviestarsApplication.java
@@ -3,11 +3,16 @@ package pl.edu.agh.pmakarewicz.moviestars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class MoviestarsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoviestarsApplication.class, args);
+        SpringApplication app = new SpringApplication(MoviestarsApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", System.getenv("PORT")));
+        app.run(args);
     }
 
 }
