95a37d26d85271c853bab2c9c2bf26bd7ff4239b
PiotrMakarewicz <makarepio@gmail.com>
2022-03-20 19:37:29 +0100

    add celeb request

--- src/main/java/pl/edu/agh/pmakarewicz/moviestars/Controller.java
+++ src/main/java/pl/edu/agh/pmakarewicz/moviestars/Controller.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.pmakarewicz.moviestars;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import freemarker.template.Template;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +31,9 @@ public class Controller {
     @Value("${serpapi_key}")
     private String serpapiKey;
 
+    @Value("${celebapi_key}")
+    private String celebapiKey;
+
     @GetMapping("/")
     public RedirectView getIndex(){
         return new RedirectView("index.html");
@@ -40,13 +45,15 @@ public class Controller {
         model.put("firstname", firstname);
         model.put("lastname", lastname);
 
-        String bio = getBio(firstname, lastname); // TODO: asynchronicznie
-        // TODO: zdjęcie z Google Photos
+        //TODO: asynchronicznie
+        String bio = getBio(firstname, lastname);
         String imageLink = getImageLink(firstname, lastname);
-        // TODO: jakieś summary z API o Filmach
+
+        Map<String, String> details = getPersonDetails(firstname, lastname);
 
         model.put("bio", bio);
         model.put("imagelink", imageLink);
+        model.putAll(details);
         try {
             Template template = configuration.getTemplate("results.html");
             Writer stringWriter = new StringWriter();
@@ -93,28 +100,66 @@ public class Controller {
         }
     }
 
-    private String performRequestToWikipedia(String firstname, String lastname) throws IOException {
-        URL url = new URL(String.format("https://en.wikipedia.org/w/api.php?titles=%s+%s&action=query&prop=extracts&explaintext=true&format=json&exsectionformat=plain&exintro=true", firstname, lastname));
-        return performRequest(url);
+    private Map<String, String> getPersonDetails(String firstname, String lastname) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            String jsonStr = performRequestToCeleb(firstname, lastname);
+//            System.out.println(jsonStr);
+            Gson gson = new Gson();
+            JsonArray rootArr = gson.fromJson(jsonStr, JsonArray.class);
+            JsonObject obj = rootArr.get(0).getAsJsonObject();
 
-    }
-    private String performRequestToSerpapi(String firstname, String lastname) throws IOException {
-        URL url = new URL(String.format("https://serpapi.com/search.json?q=%s+%s&tbm=isch&ijn=0&api_key=%s", firstname, lastname, serpapiKey));
-        return performRequest(url);
-    }
+            map.put("gender", obj.get("gender").getAsString());
+            map.put("age", obj.get("age").getAsString());
+            map.put("height", obj.get("height").getAsString());
+            map.put("nationality", obj.get("nationality").getAsString());
+            map.put("net_worth", obj.get("net_worth").getAsString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("gender", "unknown");
+            map.put("age", "unknown");
+            map.put("height", "unknown");
+            map.put("nationality", "unknown");
+            map.put("net_worth", "unknown");
 
-    private String performRequest(URL url) throws IOException {
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
         }
-        in.close();
-        con.disconnect();
-        return content.toString();
+        return map;
     }
-}
+
+        private String performRequestToWikipedia (String firstname, String lastname) throws IOException {
+            URL url = new URL(String.format("https://en.wikipedia.org/w/api.php?titles=%s+%s&action=query&prop=extracts&explaintext=true&format=json&exsectionformat=plain&exintro=true", firstname, lastname));
+            return performRequest(url);
+
+        }
+        private String performRequestToSerpapi (String firstname, String lastname) throws IOException {
+            URL url = new URL(String.format("https://serpapi.com/search.json?q=%s+%s&tbm=isch&ijn=0&api_key=%s", firstname, lastname, serpapiKey));
+            return performRequest(url);
+        }
+
+        private String performRequestToCeleb (String firstname, String lastname) throws IOException {
+            URL url = new URL(String.format("https://api.celebrityninjas.com/v1/search?name=%s+%s", firstname, lastname));
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Api-Key", celebapiKey);
+            return fetchResponse(con);
+        }
+
+        private String fetchResponse (HttpURLConnection con) throws IOException {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            return content.toString();
+        }
+
+        private String performRequest (URL url) throws IOException {
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            return fetchResponse(con);
+        }
+    }
